d1b82f5f384d961ba34b88306fb69da8e4dec0cf
Vasyl <vasia08071998@gmail.com>
2020-04-27 17:32:42 +0300

    added splash screen

---
+++ app/src/main/java/com/example/cocktailapp/SplashScreenActivity.java
@@ -0,0 +1,36 @@
+package com.example.cocktailapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 1500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        ImageView imageView = findViewById(R.id.image_view_logo);
+
+        Glide
+                .with(SplashScreenActivity.this)
+                .load("https://www.thecocktaildb.com/images/logo.png")
+                .into(imageView);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
